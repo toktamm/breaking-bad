a530a4ce347a1eaec069f7b535c6038515721d6f
Toktam <ttmohebbi@gmail.com>
2021-07-01 08:32:29 +0000

    add and set query state

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
-      <Search />
+      <Search getQuery={(q) => setQuery(q)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );

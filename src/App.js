cfad25f5b94d14039aec2c01a4c5f4b2a8293fab
Toktam <ttmohebbi@gmail.com>
2021-07-01 08:28:31 +0000

    add query to the API fetch

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import './App.css';
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('true');
+
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
       console.log(result.data);
       setItems(result.data);
       setIsLoading(false);

2ce7c2dcdc79c2d03f8e23130a158262c260890c
Toktam <ttmohebbi@gmail.com>
2021-07-01 07:49:20 +0000

    fetch the data from the breaking bad API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/Header';
 import './App.css';
-import Header from './components/ui/Header'
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      console.log(result.data);
+    }
+    fetchItems;
+  }, [])
+
   return (
     <div className='container'>
       <Header />
